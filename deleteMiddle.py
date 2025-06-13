9fc0216e1eb8155ed82a900c3fad5b4ff5046faf
Sonali Sharma <sonalisharma.cn@gmail.com>
2025-06-14 00:18:43 +0530

    Add files via upload

---
+++ deleteMiddle.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Edge case: only one node
+        if not head or not head.next:
+            return None
+        
+        # Use two pointers
+        slow = head
+        fast = head
+        prev = None
+        
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Delete the middle node
+        prev.next = slow.next
+        
+        return head
